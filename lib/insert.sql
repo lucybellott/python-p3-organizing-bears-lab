51bca708821264369fe82fcb2b5d5d98325ae994
Lucy <lucybellott@gmail.com>
2024-01-29 16:31:18 -0500

    passed tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'brown','chill', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'white', 'angry', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha',8,'F','black','chill',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown',15,'F','brown','calm',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa',8,'F','black','chill',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch',8,'M','black','chill',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy',8,'F','black','chill',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL,7,'F','black','chill',False);
